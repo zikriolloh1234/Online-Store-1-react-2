792b72e32b752d97ad58c42b7e66c30e3859a074
zikriolloh1234 <sharipovzikriolloh28@fmail.com>
2025-09-28 15:30:33 +0500

    apis

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -3,7 +3,7 @@ import { AxiosToken, BaseApi } from "../app/token";
 
 export async function registerUser(userData) {
   try {
-    const response = await axios.post(`${BaseApi}/Account/register`, userData);
+    const response = await axios.post(`${BaseApi}https://store-api.softclub.tj/Account/register`, userData);
     return response.data;
   } catch (error) {
     console.error(error);
